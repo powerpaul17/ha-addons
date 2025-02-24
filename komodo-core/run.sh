14be70d6cc68cc111ce3ea5e19907617b1f3d8e8
Paul Tirk <paultirk@paultirk.com>
2025-02-24 11:32:08 +0100

    komodo: add configs for confirm dialog & user registration

--- komodo-core/run.sh
+++ komodo-core/run.sh
@@ -7,6 +7,9 @@ echoerr "Starting Komodo"
 
 export KOMODO_PASSKEY=$(bashio::config 'periphery_passkey')
 
+export KOMODO_DISABLE_CONFIRM_DIALOG=$(bashio::config 'disable_confirm_dialog')
+export KOMODO_DISABLE_USER_REGISTRATION=$(bashio::config 'disable_user_registration')
+
 export KOMODO_LOCAL_AUTH=true
 
 exec /usr/local/bin/core
