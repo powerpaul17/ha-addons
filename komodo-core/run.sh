a341e7efe389ca82d085b651ed0ec1f0876e7f0f
Paul Tirk <paultirk@paultirk.com>
2025-06-16 22:17:07 +0200

    komodo-core: wait for db & add documentdb extension

--- komodo-core/run.sh
+++ komodo-core/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+bashio::net.wait_for 5432 localhost 900
+
 set -e
 echoerr() { printf "%s\n" "$*" >&2; }
 
@@ -11,5 +13,7 @@ export KOMODO_DISABLE_CONFIRM_DIALOG=$(bashio::config 'disable_confirm_dialog')
 export KOMODO_DISABLE_USER_REGISTRATION=$(bashio::config 'disable_user_registration')
 
 export KOMODO_LOCAL_AUTH=true
+
+psql -U postgres -c "CREATE EXTENSION IF NOT EXISTS documentdb CASCADE"
 
 exec /usr/local/bin/core
